92e570b47ad2c9880dbb7b963877bdbb52df2e23
Alan Jose <alanjose10@gmail.com>
2025-04-09 12:30:29 +0100

    load and print maze

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # pacman-go
 
 pacman game in goland
+
+## Maze symbols
+
+- '#' represents a wall
+- . represents a dot
+- P represents the player
+- G represents the ghosts (enemies)
+- X represents the power up pills
--- main.go
+++ main.go
@@ -1,18 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+var maze []string
+
+func loadMaze(file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		l := s.Text()
+		maze = append(maze, l)
+	}
+
+	return nil
+}
+
+func printMaze() {
+	for _, l := range maze {
+		fmt.Println(l)
+	}
+}
 
 func main() {
 
 	// initialize game
 
 	// load resources
+	if err := loadMaze("maze01.txt"); err != nil {
+		log.Println("failed to load maze:", err)
+		return
+	}
 
 	// main game loop
 
 	for {
 
 		// update screen
+		printMaze()
 
 		// process input
 
@@ -22,7 +56,6 @@ func main() {
 
 		// check game over
 
-		fmt.Println("Hello, Pac Go!")
 		break
 
 	}
